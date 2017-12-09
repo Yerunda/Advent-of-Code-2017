fe6dfb570fca30fefda34e5a7e93f23fdeeddfcd
yerundov <9416848+Yerundov@users.noreply.github.com>
2017-12-09 20:12:12 +0100

    Removed unneccessary variable

--- src/day5/Maze.java
+++ src/day5/Maze.java
@@ -10,9 +10,9 @@ import java.util.Scanner;
  *
  */
 public class Maze {
-	
+
 	private ArrayList<Integer> maze;
-	
+
 	public Maze(String filename){
 		maze = new ArrayList<Integer>();
 		Scanner in = null;
@@ -21,22 +21,21 @@ public class Maze {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		while(in.hasNextLine()){
-			int jump = Integer.parseInt(in.nextLine());
-			maze.add(jump);
+		while(in.hasNextInt()){
+			maze.add(in.nextInt());
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Maze main = new Maze("maze.txt");
 		main.run();
 	}
-	
+
 	public void run(){
 		System.out.println("Part One:\t" + countStepsPOne());
 		System.out.println("Part Two:\t" + countStepsPTwo());
 	}
-	
+
 	private int countStepsPOne(){
 		ArrayList<Integer> tmpMaze = (ArrayList<Integer>) maze.clone();
 		int index = 0;
@@ -49,7 +48,7 @@ public class Maze {
 		}
 		return count;
 	}
-	
+
 	private int countStepsPTwo(){
 		ArrayList<Integer> tmpMaze = (ArrayList<Integer>) maze.clone();
 		int index = 0;
@@ -62,5 +61,5 @@ public class Maze {
 		}
 		return count;
 	}
-	
+
 }
